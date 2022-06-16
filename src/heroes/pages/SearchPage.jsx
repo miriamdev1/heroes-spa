ac984e5a3b80ec31d371738e32f6ff15ce78bf20
miriam <arialcoder@gmail.com>
2022-06-15 21:39:31 -0300

    hide components

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from 'query-string';
 import { useForm } from "../../../hook/useForm";
 import { HeroCard } from "../components/HeroCard";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
 
@@ -11,16 +12,21 @@ export const SearchPage = () => {
 
     // query-String
     const { q = '' } = queryString.parse(location.search);
-    console.log({ q })
+    console.log({ q });
+
+    const heroes = getHeroesByName(q);
+
+    const showSearch = (q.length === 0);
+    const showError = (q.length > 0) && heroes.length === 0;
 
     const { searchText, onInputChange } = useForm({
-        searchText: ''
+        searchText: q
     });
 
     const onSearchSubmit = (ev) => {
         ev.preventDefault();
 
-        if (searchText.trim().length <= 1) return;
+        // if (searchText.trim().length <= 1) return;
 
         navigate(`?q=${searchText.toLowerCase().trim()}`)
 
@@ -55,15 +61,30 @@ export const SearchPage = () => {
                 <div className="col-7">
                     <h4>Results</h4>
                     <hr />
+                    {/* {
+                        (q == '')
+                            ? <div className="alert alert-primary">
+                                Search a hero
+                            </div>
+                            : (heroes.length === 0)
+                            && <div className="alert alert-danger">
+                                No hero with <b>{q}</b>
+                            </div>
+                    } */}
 
-                    <div className="alert alert-primary">
+                    <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>
                         Search a hero
                     </div>
-                    <div className="alert alert-danger">
+                    <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
                         No hero with <b>{q}</b>
                     </div>
 
                     {/* <HeroCard /> */}
+                    {
+                        heroes.map(hero => (
+                            <HeroCard key={hero.id} {...hero} />
+                        ))
+                    }
                 </div>
 
 
