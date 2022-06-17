b1086fb23a76a8926737812a5b44d7fe2dc034d9
miriam <arialcoder@gmail.com>
2022-06-16 22:04:22 -0300

    add context

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,27 +1,35 @@
-import { useNavigate } from "react-router-dom";
-
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
 
+  const { login } = useContext( AuthContext );
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const onLogin = () => {
+    
+    const lastPath = localStorage.getItem('lastPath') || '/';
 
-    const handleLogin = () => {
-        navigate('/', {
-            replace: true
-        });
+    login( 'Fernando Herrera' );
+    
+    navigate( lastPath, {
+      replace: true
+    });
+  }
 
-        console.log('handleLogin');
-    }
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
 
-    return (
-        <div className="container mt-5">
-            <h1>Login</h1>
-            <hr />
-            <button className="btn btn-primary"
-                onClick={handleLogin}
-            >Login</button>
+      <button 
+        className="btn btn-primary"
+        onClick={ onLogin }
+      >
+        Login
+      </button>
 
-        </div>
-    )
+    </div>
+  )
 }
